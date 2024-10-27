f8da1bd782ee1e044a9d5b64fe503f6ef4db3448
podgeEire <conorrice@live.co.uk>
2024-10-27 22:15:52 +0000

    first commit testing commit success

---
+++ appWithInMemDb/src/main/java/com/example/appWithInMemDb/AppWithInMemDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.appWithInMemDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithInMemDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithInMemDbApplication.class, args);
+	}
+
+}
---
+++ appWithInMemDb/src/test/java/com/example/appWithInMemDb/AppWithInMemDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appWithInMemDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWithInMemDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
