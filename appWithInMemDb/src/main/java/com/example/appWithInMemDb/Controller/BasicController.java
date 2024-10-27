1839eec3d2f6a609fb37d5806f4a1459f24845f0
podgeEire <conorrice@live.co.uk>
2024-10-27 22:17:17 +0000

    first push

--- appWithInMemDb/src/main/java/com/example/appWithInMemDb/Controller/BasicController.java
+++ appWithInMemDb/src/main/java/com/example/appWithInMemDb/Controller/BasicController.java
@@ -9,7 +9,7 @@ public class BasicController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot!!";
     }
 
 
